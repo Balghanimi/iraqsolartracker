5a9e9193f69c8bb299efc507ecde11e33e7ec249
Balghanimi <35063324+Balghanimi@users.noreply.github.com>
2025-06-10 18:21:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from gui import SolarApp  
+from scraper_google_cse import search_google
+
+offers = search_google("منظومة طاقة شمسية العراق")
+for offer in offers:
+    print(offer["title"], offer["link"])
 
 if __name__ == "__main__":
     app = SolarApp()
